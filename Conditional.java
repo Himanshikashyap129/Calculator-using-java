94e216aa7c7bd2c1c7bd42a56b060f847bbf26c9
Himanshi Kashyap <124158878+Himanshikashyap129@users.noreply.github.com>
2023-05-18 16:28:39 +0530

    Conditional.java

--- Conditional.java
+++ Conditional.java
@@ -1,9 +1,3 @@
-//shortcuts-sout and psvm
-// public class Main{
-//     public static void main(String args[]){
-//         System.out.println("jai shree krishna");
-//     }
-// }
 // ------Conditionals Loops in JAVA------
 
 import java.util.*;
@@ -11,32 +5,6 @@ import java.util.*;
 public class Conditional{
     public static void main(String args[]){
        Scanner input=new Scanner(System.in);
-    //    System.out.print("enter the value of a, b and c=");
-    //    int n=input.nextInt();
-    //    int b=input.nextInt();
-    //    int c=input.nextInt();
-    //    System.out.print("Largest Number is =");
-    //    int max=a;
-    //    if(b>max){
-    //     max=b;
-    //    }
-    //    if(c>max){
-    //     max=c;
-    //    }
-    //    System.out.println(max);
-    // int max=Math.max(c, Math.max(a, b));      class for find the maximum number
-    // System.out.println(max);
-    // int rem;
-    // int count=0;
-    // while(n>0){
-    //     rem=n%10;
-    //     if(rem==3){             //Counting occurence of the number
-    //         count++;
-    //     }
-    //   n=n/10;
-    // }
-    // System.out.println(count);
-    //take input from the user till user does not press X or x
     int ans=0;
     System.out.print("Enter the operator:");
     while(true){
